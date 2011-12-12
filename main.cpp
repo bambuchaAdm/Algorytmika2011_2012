cbcc93b01f15d26cb340c13c47211ccdf2b65887
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2011-12-12 22:33:42 +0100

    Segfault z jakiejs cholery.

--- main.cpp
+++ main.cpp
@@ -4,28 +4,29 @@
 
 using namespace std;
 
-const int MAX = 10;
+const long long MAX = 1000001;
 
 int remontuj()
 {
 	int ldziur=0;
 	scanf("%d",&ldziur);
-	int koszt[MAX+1]={0};
-	int wspolrzedna[MAX+1]={0};
+	int koszt[MAX]={0};
+	int wspolrzedna[MAX]={0};
 	for(int i=1;i<=ldziur;i++)
 	{
 		scanf("%d %d",&wspolrzedna[i],&koszt[i]);
     }
 	int dllaty=0, cenalaty=0;
 	scanf("%d %d",&dllaty,&cenalaty);
-	int pomoc[MAX+1] = {0};
-
+	int pomoc[MAX] = {0};
+    int ost=0;
 	for(int i=0;i<=ldziur;i++)
 	{
 		int latamy, nielatamy;
 		nielatamy = koszt[i] + pomoc[i-1];
-		int zas = lower_bound(&wspolrzedna[0],&wspolrzedna[MAX+1],wspolrzedna[i]-dllaty) - &wspolrzedna[0];
-		latamy = cenalaty + pomoc[zas];
+        while(wspolrzedna[ost]<wspolrzedna[i]-dllaty)
+            ost++;
+		latamy = cenalaty + pomoc[ost-1];
 		pomoc[i] = min(latamy,nielatamy);
 	}
 
