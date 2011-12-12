67ab040d681eff59e0f05b91d17e0b6aedbf17ac
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2011-12-12 13:42:59 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 10;
+
+int remontuj()
+{
+	int ldziur=0;
+	scanf("%d",&ldziur);
+	int koszt[MAX+1];
+	int wspolrzedna[MAX+1];
+	koszt[0]=0;
+	wspolrzedna[0]=0;
+	for(int i=1;i<=ldziur;i++)
+	{
+		scanf("%d %d",&wspolrzedna[i],&koszt[i]);
+	}
+	int dllaty=0, cenalaty=0;
+	scanf("%d %d",&dllaty,&cenalaty);
+
+	int pomoc[MAX+1] = {0};
+
+	for(int i=1;i<ldziur;i++)
+	{
+		int latamy, nielatamy;
+		nielatamy = koszt[i] + pomoc[i-1];
+		int zas = lower_bound(&wspolrzedna[0],&wspolrzedna[MAX+1],wspolrzedna[i]-dllaty) - &wspolrzedna[0];
+		latamy = cenalaty + pomoc[zas];
+		pomoc[i] = min(latamy,nielatamy);
+	}
+
+	return pomoc[ldziur-1];
+}
+
+int main()
+{
+	int z=0;
+	scanf("%d",&z);
+	for(int i=0;i<z;i++)
+		printf("%d\n",remontuj());
+	return 0;
+}
