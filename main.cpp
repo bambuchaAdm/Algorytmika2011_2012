0a6b4f581ad6617299af1ad3e4d50fb343633c43
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2011-12-18 00:10:11 +0100

    Debugguje odtwarzanie sciezki.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ const int MAX = 1001;
 struct pole
 {
     int wartosc;
-    bool kierunek;
+    short int kierunek;
 };
 
 pole pomoc[MAX][MAX];
@@ -21,9 +21,9 @@ void zbieraj_diamenty()
     int szachownica[MAX][MAX];
 
     for(int i=0;i<=kolumny;i++)
-        pomoc[0][i].wartosc=0;
+        pomoc[0][i].wartosc=pomoc[0][i].kierunek=0;
     for(int i=0;i<=wiersze;i++)
-        pomoc[i][0].wartosc=0;
+        pomoc[i][0].wartosc=pomoc[i][0].kierunek=0;
 
     for(int i=1;i<=wiersze;i++)
         for(int j=1;j<=kolumny;j++)
@@ -40,15 +40,15 @@ void zbieraj_diamenty()
 
             a = pomoc[i][j-1].wartosc;
             b = pomoc[i-1][j].wartosc;
-            if(a>b)
+            if(a>=b)
             {
                 pomoc[i][j].wartosc+=a;
-                pomoc[i][j].kierunek=true;
+                pomoc[i][j].kierunek=1;
             }
             else
             {
                 pomoc[i][j].wartosc+=b;
-                pomoc[i][j].kierunek=false;
+                pomoc[i][j].kierunek=2;
             }
         }
         cout << pomoc[wiersze][kolumny].wartosc << endl;
@@ -56,18 +56,25 @@ void zbieraj_diamenty()
         for(int i=0;i<kolumny+wiersze-2;i++)
         {
             int x=kolumny, y=wiersze;
-            if(pomoc[x][y].kierunek==true)
+            if(pomoc[x][y].kierunek==1)
             {
-                sciezka = sciezka + 'v';
+                sciezka = sciezka + ">";
                 x--;
             }
-            else
+            else if(pomoc[x][y].kierunek==2)
             {
-                sciezka = sciezka + '>';
+                sciezka = sciezka + "v";
                 y--;
             }
         }
-        cout << sciezka;
+        cout << sciezka << endl;
+
+        for(int i=0;i<=wiersze;i++)
+        {
+            for(int j=0;j<=kolumny;j++)
+                cout << pomoc[i][j].kierunek << " ";
+            cout << endl;
+        }
 }
 
 int main()
