2ef1a04a4ecdf0e31f47631e3bac681b1316588b
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2011-12-18 14:47:55 +0100

    Dodalem wczytanie danych.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
 
 using namespace std;
 
+const int MAX = 3001;
+
+int pomoc[MAX][MAX] =  {{0}};
+
+int liczpodciag()
+{
+    char obecny[MAX];
+    char docelowy[MAX];
+    scanf("%s %s",obecny,docelowy);
+
+    return -1;
+}
+
+void odtworz_sciezke()
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int z=0;
+    scanf("%d",&z);
+    for(int i=0;i<z;i++)
+    {
+        printf("%d\n",liczpodciag());
+        odtworz_sciezke();
+    }
 }
