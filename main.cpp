a27fc951ca6c297a8a58781b7c3ab27097ebe585
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2011-12-18 14:14:14 +0100

    Walka z bugiem odtwarzania sciezki. Przerobki ustalania wartosci - 1 z gory, 2 s lewa.

--- main.cpp
+++ main.cpp
@@ -17,55 +17,60 @@ pole pomoc[MAX][MAX];
 void zbieraj_diamenty()
 {
     int kolumny=0, wiersze=0;
-    scanf("%d %d",&kolumny,&wiersze);
+    cin >> kolumny >> wiersze;
     int szachownica[MAX][MAX];
 
     for(int i=0;i<=kolumny;i++)
-        pomoc[0][i].wartosc=pomoc[0][i].kierunek=0;
+            pomoc[0][i].wartosc=pomoc[0][i].kierunek=0;
     for(int i=0;i<=wiersze;i++)
-        pomoc[i][0].wartosc=pomoc[i][0].kierunek=0;
-
+            pomoc[i][0].wartosc=pomoc[i][0].kierunek=0;
     for(int i=1;i<=wiersze;i++)
         for(int j=1;j<=kolumny;j++)
-            scanf("%d",&szachownica[i][j]);
+            cin >> szachownica[i][j];
 
     for(int i=1;i<=wiersze;i++)
         for(int j=1;j<=kolumny;j++)
         {
-            int a=0,b=0;
+            int lewo=0,gora=0;
             if(szachownica[i][j])
                 pomoc[i][j].wartosc=1;
             else
                 pomoc[i][j].wartosc=0;
 
-            a = pomoc[i][j-1].wartosc;
-            b = pomoc[i-1][j].wartosc;
-            if(a>=b)
+            gora = pomoc[i][j-1].wartosc;
+            lewo = pomoc[i-1][j].wartosc;
+            if(gora < lewo)
             {
-                pomoc[i][j].wartosc+=a;
-                pomoc[i][j].kierunek=1;
+
+                pomoc[i][j].kierunek = 1;
+                pomoc[i][j].wartosc +=lewo;
             }
             else
             {
-                pomoc[i][j].wartosc+=b;
-                pomoc[i][j].kierunek=2;
+                if(j==1)
+                    pomoc[i][j].kierunek = 1;
+                else
+                    pomoc[i][j].kierunek = 2;
+                pomoc[i][j].wartosc += gora;
             }
         }
-        cout << pomoc[wiersze][kolumny].wartosc << endl;
+        cout << pomoc[wiersze][kolumny].wartosc << " ";
         string sciezka;
+        int x=wiersze, y=kolumny;
         for(int i=0;i<kolumny+wiersze-2;i++)
         {
-            int x=kolumny, y=wiersze;
             if(pomoc[x][y].kierunek==1)
             {
-                sciezka = sciezka + ">";
+                sciezka = "v" + sciezka;
                 x--;
             }
             else if(pomoc[x][y].kierunek==2)
             {
-                sciezka = sciezka + "v";
+                sciezka = ">" + sciezka;
                 y--;
             }
+
+            cout << x << " " << y << endl;
         }
         cout << sciezka << endl;
 
@@ -75,12 +80,20 @@ void zbieraj_diamenty()
                 cout << pomoc[i][j].kierunek << " ";
             cout << endl;
         }
+
+        cout << endl;
+        for(int i=0;i<=wiersze;i++)
+        {
+            for(int j=0;j<=kolumny;j++)
+                cout << pomoc[i][j].wartosc << " ";
+            cout << endl;
+        }
 }
 
 int main()
 {
     int z=0;
-    scanf("%d",&z);
+    cin >> z;
     for(int i=0;i<z;i++)
         zbieraj_diamenty();
     return 0;
