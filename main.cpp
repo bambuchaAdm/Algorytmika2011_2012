bce6212433b70b73392a7937210f6d214fc5aa3a
ArcCha <adam1piekarczyk@gmail.com>
2012-01-03 15:41:09 +0100

    Klepniete, mniej wiecej dziala. Trzeba poszukach bugow.

--- main.cpp
+++ main.cpp
@@ -5,19 +5,26 @@
 using namespace std;
 
 const int MAX = 100000;
+const int k8=2000000001;
 
 void ustawiaj()
 {
     int stan=0;
     scanf("%d",&stan);
-    int kompania[MAX];
+    int kompania[MAX]={-k8};
+    for(int i=0;i<MAX;i++)
+        kompania[i]=k8;
     for(int i=0;i<stan;i++)
     {
         int zolniez=0;
         scanf("%d",&zolniez);
         int indeks = (int) (upper_bound(&kompania[0],&kompania[MAX],zolniez) - &kompania[0]);
-
+        kompania[indeks]=zolniez;
     }
+    int q=1;
+    while(kompania[q]!=k8)
+        q++;
+    printf("%d\n",stan-q);
 }
 
 int main()
