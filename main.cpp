cd09c19e4d06cb75d8cc7fafc86da9f8a4d52d0c
ArcCha <adam1piekarczyk@gmail.com>
2012-01-03 15:27:02 +0100

    Wklepany schemat, zabieram sie za algorytm.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
+const int MAX = 100000;
+
+void ustawiaj()
+{
+    int stan=0;
+    scanf("%d",&stan);
+    int kompania[MAX];
+    for(int i=0;i<stan;i++)
+    {
+        int zolniez=0;
+        scanf("%d",&zolniez);
+        int indeks = (int) (upper_bound(&kompania[0],&kompania[MAX],zolniez) - &kompania[0]);
+
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int z=0;
+    scanf("%d",&z);
+    for(int i=0;i<z;i++)
+    {
+        ustawiaj();
+    }
     return 0;
 }
