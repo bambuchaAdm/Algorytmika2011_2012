295b1f2164819f56b52faf8a5e891e39ff2ea45f
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-01-09 22:39:54 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 101;
+
+void strzelaj()
+{
+    bool spotyka[MAX][MAX];
+    bool wygrywa[MAX][MAX];
+    int lmusz=0;
+    scanf("%d",&lmusz);
+    for(int i=0;i<lmusz;i++)
+        for(int j=0;j<lmusz;j++)
+            cin >> wygrywa[i][j];
+}
+
+int main()
+{
+    int z=0;
+    scanf("%d",&z);
+    for(int i=0;i<z;i++)
+        strzelaj();
+    return 0;
+}
