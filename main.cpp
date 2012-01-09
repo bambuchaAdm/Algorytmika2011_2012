d7f9ebbbbe4c15edf2e169cc396a09c55edc7fd2
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-01-09 22:54:52 +0100

    Zaklepalem cos co powinno dzialac.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,6 +14,35 @@ void strzelaj()
     for(int i=0;i<lmusz;i++)
         for(int j=0;j<lmusz;j++)
             cin >> wygrywa[i][j];
+
+    for(int i=0;i<lmusz;i++)
+        spotyka[i][(i+1)%lmusz]=true;
+    for(int i=2;i<lmusz;i++)
+        for(int j=0;j<lmusz;j++)
+        {
+            int skraj=i+j;
+            spotyka[i][skraj%lmusz]=false;
+            for(int k=j+1;k<skraj-1;k++)
+                if((wygrywa[j][k%nlmusz] || wygrywa[skraj%lmusz][k%lmusz]) && spotyka[j][k%lmusz] && spotyka[k%lmusz][skraj%lmusz])
+                    spotyka[j][skraj%lmusz]=true;
+        }
+
+    vector<int> wygrani;
+    for(int i=0;i<lmusz;i++)
+        for(int j=0;j<lmusz;j++)
+        {
+            if(spotyka[i][j] && spotyka[j][i])
+                if(wygrywa[i][j])
+                    wygrani.push_back(i);
+                else
+                    wygrani.push_back(j);
+        }
+    printf("%d\n",wygrani.size());
+    while(!wygrani.empty())
+    {
+        printf("%d\n",wygrani.back());
+        wygrani.pop_back();
+    }
 }
 
 int main()
