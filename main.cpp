fab49aafe51a98888942645fc0c4eb24031f6ef6
ArcCha <adam1piekarczyk@gmail.com>
2012-01-17 15:21:13 +0100

    Klepnąłem kilka przydatnych funkcji i ogólny schemat algorytmu.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 const int MAX = 201;
 
 int lwierzch;
-pair wierch[MAX];
+pair<int,int> wierch[MAX];
 
 void wczytaj()
 {
@@ -16,15 +16,35 @@ void wczytaj()
         cin >> wierch[i].first >> wierch[i].second;
 }
 
+int do2(int a)
+{
+    return a*a;
+}
+
+int odleglosc(pair<int,int> w, pair<int,int> w1)
+{
+    return do2(w1.first-w.first) + do2(w1.second - w.second);
+}
+
 void trianguluj()
 {
-    int wynik[MAX][MAX]
+    int wynik[MAX][MAX];
     for(int i=0;i<lwierzch;i++)
         wynik[i][i]=0;
     for(int i=0;i<lwierzch;i++)
         wynik[i][(i+1)%lwierzch]=0;
     for(int i=0;i<lwierzch;i++)
         wynik[i][(i+2)%lwierzch]=0;
+    for(int i=0;i<lwierzch;i++)
+    {
+        for(int j=3;i<lwierzch;i++)     //ew <=
+        {
+            for(int k=i;k<i+j;k++)
+            {
+
+            }
+        }
+    }
 }
 
 int main()
