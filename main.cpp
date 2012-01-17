04632bca5c90109ada0d61fbaa2cda69875860e5
ArcCha <adam1piekarczyk@gmail.com>
2012-01-17 15:13:10 +0100

    intial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+const int MAX = 201;
+
+int lwierzch;
+pair wierch[MAX];
+
+void wczytaj()
+{
+    cin >> lwierzch;
+    for(int i=0;i<lwierzch;i++)
+        cin >> wierch[i].first >> wierch[i].second;
+}
+
+void trianguluj()
+{
+    int wynik[MAX][MAX]
+    for(int i=0;i<lwierzch;i++)
+        wynik[i][i]=0;
+    for(int i=0;i<lwierzch;i++)
+        wynik[i][(i+1)%lwierzch]=0;
+    for(int i=0;i<lwierzch;i++)
+        wynik[i][(i+2)%lwierzch]=0;
+}
+
+int main()
+{
+    int z;
+    cin >> z;
+    for(int i=0;i<z;i++)
+    {
+        wczytaj();
+        trianguluj();
+    }
+    return 0;
+}
