5eb81b3fcc7556352353dde720b4a5c172c42c38
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-03-17 12:59:57 +0100

    Pisze procedure liczkwadraty.

--- D11/main.cpp
+++ D11/main.cpp
@@ -12,12 +12,12 @@ struct punkt
 int szer, wys;
 
 vector< vector<bool> > pole;
-vector< vecotor<punkt> > d; //distance
+vector< vector<punkt> > d; //distance
 
 void readInput()
 {
     cin >> wys >> szer;
-    pole.resize(wys+1,vector<int>(szer+1));
+    pole.resize(wys+1,vector<bool>(szer+1));
     d.resize(wys+1,vector<punkt>(szer+1));
     for(int i=0;i<=szer;i++)
         pole[0][i] = 0;
@@ -44,7 +44,7 @@ void liczOdleglosci()
 {
     for(int i=1;i<=wys;i++)
     {
-        for(int j=1;k<=szer;j++)
+        for(int j=1;j<=szer;j++)
         {
             if(pole[i][j]==true)
             {
@@ -53,8 +53,8 @@ void liczOdleglosci()
             }
             else
             {
-                d[i][j].gora = d[i-1][j] + 1;
-                d[i][j].lewo = d[i][j-1] + 1;
+                d[i][j].gora = d[i-1][j].gora + 1;
+                d[i][j].lewo = d[i][j-1].lewo + 1;
             }
         }
     }
@@ -62,11 +62,20 @@ void liczOdleglosci()
 
 void liczKwadraty()
 {
+    punkt wynik; //wykorzystam sobie strukta do przechowywania wyniku. Gora - jak duzy, lewo - ile.
+    wynik.gora = wynik.lewo = 0;
     for(int i=1;i<=wys;i++)
     {
         for(int j=1;j<=szer;j++)
         {
 
+            int maxodl = 0;
+            maxodl = min(d[i][j].gora, d[i][j].lewo);
+            maxodl--;
+            for(int k=0;k<maxodl;k++)
+            {
+
+            }
         }
     }
 }
@@ -75,7 +84,7 @@ int main()
 {
     int z;
     cin >> z;
-    for(int i=0;i<z;i<++)
+    for(int i=0;i<z;i++)
     {
         readInput();
         liczOdleglosci();
