ccf42c3488e144abacb22c3b1255aae11d4de5ce
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-03-18 16:53:03 +0100

    Modyfikacja gitignore i dodanie D10.

---
+++ seriaD/D10/main.cpp
@@ -0,0 +1,47 @@
+/**
+*   Author: Adam Piekarczyk
+*   Project Title:
+*   School Year: 2011/2012
+*   School: VLO Kraków
+*   Class: 1e
+**/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+void readInput()
+{
+
+}
+
+void printOutput()
+{
+
+}
+
+void clean()
+{
+
+}
+
+void test()
+{
+
+}
+
+int main()
+{
+    int z;
+    scanf("%d",&z);
+    for(int i=0;i<z;i++)
+    {
+        readInput();
+
+        printOutput();
+        //test();
+        clean();
+    }
+}
