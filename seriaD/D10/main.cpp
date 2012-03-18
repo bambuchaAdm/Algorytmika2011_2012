c60cb1831908b4d7250e98087db7bb19cd7f97cd
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-03-18 16:57:57 +0100

    Wczytywanie danych D10 zaklepane.

--- seriaD/D10/main.cpp
+++ seriaD/D10/main.cpp
@@ -1,6 +1,6 @@
 /**
 *   Author: Adam Piekarczyk
-*   Project Title:
+*   Project Title: D10 - Naszyjnik
 *   School Year: 2011/2012
 *   School: VLO Kraków
 *   Class: 1e
@@ -12,9 +12,21 @@
 
 using namespace std;
 
+int lkoral;
+
+vector<vector<char> > naszyjnik;
+vector<vector<bool> > ok;
+
 void readInput()
 {
-
+    scanf("%d",&lkoral);
+    naszyjnik.resize(lkoral+1,vector<char>(2));
+    for(int i=1;i<=lkoral;i++)
+    {
+        scanf("%c %c",&naszyjnik[i][0],&naszyjnik[i][1]);
+    }
+    naszyjnik[0][0] = naszyjnik[lkoral][0];
+    naszyjnik[0][1] = naszyjnik[lkoral][1];
 }
 
 void printOutput()
