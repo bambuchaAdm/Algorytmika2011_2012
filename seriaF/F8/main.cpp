730e8a4106793e11d17fd38b37bcdac867f79ea7
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-03-25 21:08:26 +0200

    Dodanie funkcji porównujących.

--- seriaF/F8/main.cpp
+++ seriaF/F8/main.cpp
@@ -15,9 +15,44 @@
 
 using namespace std;
 
+struct kabel
+{
+    int x;
+    int y;
+    int z;
+    char znak;
+}
+
+int dlx,dly,dlz; //wymiary prostokata
+int lkabli;
+
+vector<kabel> kable;
+
+bool compareZ(kabel a, kable b)
+{
+    return a.z < b.z;
+}
+
+bool compareY(kabel a, kable b)
+{
+    return a.y < b.y;
+}
+
+bool compareX(kabel a, kable b)
+{
+    return a.x < b.x;
+}
+
 void readInput()
 {
-
+    cin >> x >> y >> z;
+    cin >> lkabli;
+    const int MAX = lkabli;
+    kable.resize(MAX);
+    for(int i=0;i<lkabli;i++)
+    {
+        cin >> kable[i].x >> kable[i].y >> kable[i].z >> kable[i].znak;
+    }
 }
 
 void printOutput()
@@ -37,6 +72,7 @@ void test()
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
     int z;
     scanf("%d",&z);
     for(int i=0;i<z;i++)
