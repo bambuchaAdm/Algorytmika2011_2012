09d563281601dc2ccca19eee4447faeadf2aec02
Adam Piekarczyk <adam1piekarczyk@gmail.com>
2012-04-15 19:21:52 +0200

    Coś spaprałem, RTE.

--- seriaG/G1/main.cpp
+++ seriaG/G1/main.cpp
@@ -12,7 +12,7 @@
 #include <string>
 #include <cstring>
 
-//#define NDEBUG
+#define NDEBUG
 
 using namespace std;
 
@@ -31,26 +31,18 @@ int minPeriod(const char *s)
     ps[0] = -1; //czemu? dla wygody, ale nie jeste pewien
     ps[1] = 0;
     int idx = 0; //lazimy tym po wzorcu
-    for(int i=2;i<strlen(s);i++)
+    for(int i=2; s[i]; i++)
     {
-        if(s[i-1]==s[idx])
-        {
-            ps[i] = idx + 1;
-            i++;
-            idx++;
-        }
-        else if(idx > 0)
+        while(idx > 0 && s[idx+1] != s[i])
             idx = ps[idx];
-        else
-        {
-            ps[i] = 0;
-            i++;
-        }
+        if(s[idx+1]==s[i])
+            idx++;
+        ps[i] = idx;
     }
     #ifndef NDEBUG
     test(ps,strlen(s));
     #endif
-    return strlen(s)/(strlen(s) - ps[strlen(s)]);
+    return strlen(s)/(strlen(s) - ps[strlen(s)-1] -1);
 }
 
 void readInput()
